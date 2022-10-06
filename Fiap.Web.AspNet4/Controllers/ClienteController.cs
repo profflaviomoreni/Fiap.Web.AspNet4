394e99b13707e71278a0d5ab3297b21269375c5e
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-10-05 21:45:17 -0300

    first commit

---
+++ Fiap.Web.AspNet4/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using Fiap.Web.AspNet4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet4.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            return View();
+            //return Content("Fiap Asp.NET Core");
+            //return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Novo(ClienteModel clienteModel)
+        {
+            Console.WriteLine(clienteModel.Nome);
+            Console.WriteLine(clienteModel.Sobrenome);
+
+            // ClasseBancoDados.Insert( clienteModel );
+
+            return View("Sucesso");
+        }
+
+    }
+}
---
+++ Fiap.Web.AspNet4/Models/ClienteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiap.Web.AspNet4.Models
+{
+    public class ClienteModel
+    {
+        public int ClienteId { get; set; }
+        public string? Nome { get; set; }
+        public string? Sobrenome { get; set; }
+    }
+}
