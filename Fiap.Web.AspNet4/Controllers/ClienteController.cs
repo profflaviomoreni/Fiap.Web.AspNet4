b7cde0464933718afd0001208781314ec0225deb
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-10-10 21:57:52 -0300

    finalizando fluxo de navegação

--- Fiap.Web.AspNet4/Controllers/ClienteController.cs
+++ Fiap.Web.AspNet4/Controllers/ClienteController.cs
@@ -8,7 +8,48 @@ namespace Fiap.Web.AspNet4.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            // var resultado = SELECT * FROM tabelaCliente;
+
+            var listaClientes = new List<ClienteModel>();
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 1,
+                Nome = "Flávio",
+                Email = "fmoreni@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS1"
+            });
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 2,
+                Nome = "Eduardo",
+                Email = "eduardo@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS3"
+            });
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 3,
+                Nome = "Moreni",
+                Email = "moreni@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS3"
+            });
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 4,
+                Nome = "Luan",
+                Email = "luan@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS4"
+            });
+
+
+            //ViewBag.Clientes = listaClientes;
+            //ViewData["Clientes"] = listaClientes;
+            //TempData["Clientes"] = listaClientes;
+
+            return View(listaClientes);
         }
 
         [HttpGet]
@@ -27,7 +68,117 @@ namespace Fiap.Web.AspNet4.Controllers
 
             // ClasseBancoDados.Insert( clienteModel );
 
-            return View("Sucesso");
+            TempData["Mensagem"] = "Cliente inserido com sucesso";
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            // var cliente = "SELECT ... FROM cliente WHERE Id = {id}";
+            var clienteModel = new ClienteModel();
+
+            if (id == 1)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 1,
+                    Nome = "Flavio",
+                    Email = "fmoreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS1"
+                };
+            }
+            else if (id == 2)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 2,
+                    Nome = "Eduardo",
+                    Email = "eduardo@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            else
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 3,
+                    Nome = "Moreni",
+                    Email = "moreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+
+            return View(clienteModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Editar(ClienteModel clienteModel)
+        {
+            // UPDATE tabela VALUES ...
+            Console.WriteLine(clienteModel.Email);
+
+            TempData["Mensagem"] = "Cliente editado com sucesso";
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Remover(int id)
+        {
+            TempData["Mensagem"] = "Cliente removido com sucesso";
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Detalhe(int id)
+        {
+            var clienteModel = new ClienteModel();
+
+            if (id == 1)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 1,
+                    Nome = "Flavio",
+                    Email = "fmoreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS1"
+                };
+            }
+            else if (id == 2)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 2,
+                    Nome = "Eduardo",
+                    Email = "eduardo@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            else
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 3,
+                    Nome = "Moreni",
+                    Email = "moreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+
+            return View(clienteModel);
         }
 
     }
--- Fiap.Web.AspNet4/Models/ClienteModel.cs
+++ Fiap.Web.AspNet4/Models/ClienteModel.cs
@@ -5,5 +5,8 @@
         public int ClienteId { get; set; }
         public string? Nome { get; set; }
         public string? Sobrenome { get; set; }
+        public string? Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Observacao { get; set; }
     }
 }
