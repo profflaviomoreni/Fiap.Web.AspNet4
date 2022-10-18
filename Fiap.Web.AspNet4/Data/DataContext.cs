74532031087a94a865c2f8bad18f20d8112c1b48
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-10-17 23:45:24 -0300

    Iniciando EF

---
+++ Fiap.Web.AspNet4/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Fiap.Web.AspNet4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Web.AspNet4.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<RepresentanteModel> Representantes { get; set; }
+
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DataContext()
+        {
+        }
+    }
+}
---
+++ Fiap.Web.AspNet4/Migrations/20221018004257_Inicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiap.Web.AspNet4.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Representante",
+                columns: table => new
+                {
+                    RepresentanteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeRepresentante = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Representante", x => x.RepresentanteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Representante");
+        }
+    }
+}
